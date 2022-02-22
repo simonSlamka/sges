002ca48ae5c16f0b5237b3f859add9c9300f3d7e
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2022-02-22 19:08:41 +0100

    xcode proj init;

---
+++ Existence Score/Existence Score/Existence_ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Existence_ScoreApp.swift
+//  Existence Score
+//
+//  Created by Simon Slamka on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Existence_ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
